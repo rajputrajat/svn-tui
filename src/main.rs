be0b83076ed8ae5b78d66d569605bab4db612eef
Rajat Rajput <rajputrajat@gmail.com>
2022-02-08 22:57:10 +0530

    a simple block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
+
+fn main() -> Result<(), io::Error> {
+    env_logger::init();
+
+    // start terminal mode
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|frame| {
+        let size = frame.size();
+        let block = Block::default().title("block").borders(Borders::ALL);
+        frame.render_widget(block, size);
+    })?;
+    thread::sleep(Duration::from_secs(5));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
