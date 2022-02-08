b10793a531cedc4d3faf06e7723686f50d0d707d
Rajat Rajput <rajputrajat@gmail.com>
2022-02-08 23:00:25 +0530

    extract out the functin

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,10 @@ use tui::{
 
 fn main() -> Result<(), io::Error> {
     env_logger::init();
+    ui()
+}
 
+fn ui() -> Result<(), io::Error> {
     // start terminal mode
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -25,6 +28,7 @@ fn main() -> Result<(), io::Error> {
         let block = Block::default().title("block").borders(Borders::ALL);
         frame.render_widget(block, size);
     })?;
+
     thread::sleep(Duration::from_secs(5));
 
     // restore terminal
