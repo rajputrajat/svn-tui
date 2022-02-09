9d236d74041d53955a01315655bc8914b998c426
Rajat Rajput <rajputrajat@gmail.com>
2022-02-09 13:51:09 +0530

    implementing values fetcher

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use log::info;
-use std::{io, thread, time::Duration};
+use std::{
+    io,
+    sync::mpsc::{Receiver, Sender},
+    thread,
+    time::Duration,
+};
 use svn_cmd::{Credentials, SvnCmd, SvnError};
 use tui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Style},
-    widgets::{Block, BorderType, Borders, List, ListItem},
+    widgets::{Block, BorderType, Borders, List, ListItem, ListState},
     Terminal,
 };
 
@@ -94,6 +99,86 @@ fn ui() -> Result<(), CustomError> {
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+struct RequesterHandle<T> {
+    handle: Box<dyn Fn(&T)>,
+    recv_ch: Receiver<Vec<T>>,
+}
+
+struct CustomList<T> {
+    items: Vec<T>,
+    state: ListState,
+    req_hndl: Option<RequesterHandle<T>>,
+}
+
+impl<T: Clone> CustomList<T> {
+    fn set_request_handle(&mut self, hndl: RequesterHandle<T>) {
+        self.req_hndl.replace(hndl);
+    }
+
+    fn add_items(&mut self, items: &[T]) {
+        self.items.extend_from_slice(items);
+    }
+
+    fn replace_items(&mut self, items: Vec<T>) {
+        self.items = items;
+    }
+
+    fn next(&mut self) {
+        if let Some(cur) = self.state.selected() {
+            if self.items.len() + 1 > cur {
+                self.state.select(Some(cur + 1));
+            } else {
+                self.state.select(Some(0));
+            }
+        }
+    }
+
+    fn prev(&mut self) {
+        if let Some(cur) = self.state.selected() {
+            if cur > 1 {
+                self.state.select(Some(cur - 1));
+            } else {
+                self.state.select(Some(self.items.len() - 1))
+            }
+        }
+    }
+
+    fn selected(&self) {
+        if let Some(hndl) = &self.req_hndl {
+            (hndl.handle)(self.items.get(self.state.selected().unwrap()).unwrap());
+        }
+    }
+}
+
+impl<T: Clone> From<&[T]> for CustomList<T> {
+    fn from(items: &[T]) -> Self {
+        CustomList::from(items.to_vec())
+    }
+}
+
+impl<T> From<Vec<T>> for CustomList<T> {
+    fn from(items: Vec<T>) -> Self {
+        let mut v = Self {
+            items,
+            ..Default::default()
+        };
+        if !v.items.is_empty() {
+            v.state.select(Some(0));
+        }
+        v
+    }
+}
+
+impl<T> Default for CustomList<T> {
+    fn default() -> Self {
+        Self {
+            items: vec![],
+            state: Default::default(),
+            req_hndl: None,
+        }
+    }
 }
 
 #[derive(Debug)]
